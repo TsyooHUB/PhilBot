2ac13eb0c5b8ae6341dff610fbaf72b37ab790bc
tuturuam <aizadmoh@gmail.com>
2020-04-23 14:46:34 -0400

    implements juan meme

--- PhilBot.py
+++ PhilBot.py
@@ -1,12 +1,21 @@
+import io
+import discord
+import aiohttp
 import discord
 import os.path
+
+from PIL import Image
 
 BOT_PREFIX = ("!")
 TOKEN = ''
 
 client = discord.Client()
-philbank = []
 
+COORDS = [(631, 551), (990, 550), (634, 308), (980, 339)]
+COEFFS = [  0.6547747129877984, 0.008083638431858402, -1670.4677146851654, -0.05607509889913542,
+            0.6258704586809742, -628.8659542867306, -7.95610829238863e-05, 4.137514033619954e-05]
+
+philbank = []
 
 class Register:
     def __init__(self, user_id, balance):
@@ -25,13 +34,11 @@ class Register:
     def get_balance(self):
         return self.balance
 
-
 def get_user_register(user_id):
     for register in philbank:
         if register.get_id() == user_id:
             return f"User: {get_name_from_users(register.get_id())}, Balance: {register.get_balance()}"
     return "Philbank not found"
-
 
 def add_philcoin(user_id, amount):
     for register in philbank:
@@ -42,7 +49,6 @@ def add_philcoin(user_id, amount):
     new_register = Register(user_id, amount)
     philbank.append(new_register)
     save_philbank()
-
 
 def get_philcoin_balance(user_id):
     for register in philbank:
@@ -64,7 +70,6 @@ def save_philbank():
             f.write("{'Name': '" + get_name_from_users(register.get_id()) + "','UserId': " + str(register.get_id())
                     + ",'Balance': " + str(register.get_balance()) + "}\n")
 
-
 def process_command(command):
     if command.author.id == client.user.id:
         return False
@@ -76,13 +81,39 @@ def process_command(command):
     print(f'Weird command failure: {command.content}')
     return False
 
+def process_image(buffer):
+    width, height = 1219, 684
+    im = Image.open(buffer).convert(mode='RGBA')
+    im = im.resize((width, height), resample=Image.ANTIALIAS)
+    bg = Image.open('./juan_bg.png').convert(mode='RGBA')
+    im = im.transform((width * 4, height * 4), Image.PERSPECTIVE, COEFFS, Image.BICUBIC)
+    im = im.resize((bg.width, bg.height), resample=Image.ANTIALIAS)
+
+    bg = Image.alpha_composite(bg, im)
+
+    fg = Image.open('./juan_fg.png').convert(mode='RGBA')
+    bg = Image.alpha_composite(bg, fg)
+
+    output = io.BytesIO()
+    bg.save(output, format='png')
+    output.seek(0)
+    return output
 
 @client.event
 async def on_message(message):
-    command = process_command(message)
-    if command == "philbalance":
-        await message.author.send(get_user_register(message.author.id))
+    proc = process_command(message)
+    if proc != False:
+        command = proc.split()
+        if command[0] == "philbalance":
+            await message.author.send(get_user_register(message.author.id))
+        if command[0] == "juan":
+            # in an async function, such as an on_message handler or command
+            async with aiohttp.ClientSession() as session:
+                # note that it is often preferable to create a single session to use multiple times later - see below for this.
+                async with session.get(command[1]) as resp:
+                    buffer = io.BytesIO(await resp.read())
 
+            await message.channel.send(file=discord.File(fp=process_image(buffer), filename="image.png"))
 
 @client.event
 async def on_reaction_add(reaction, user):
@@ -95,12 +126,12 @@ async def on_reaction_add(reaction, user):
 async def on_ready():
     print('Phil Bot running')
 
-    if not os.path.exists('philbank.txt'):
-        open('philbank.txt', 'w')
+    if not os.path.exists('philbank.txt'):	
+        open('philbank.txt', 'w')	
 
-    with open("philbank.txt", "r") as f:
-        for line in f:
-            new_register = eval(line)
+    with open("philbank.txt", "r") as f:	
+        for line in f:	
+            new_register = eval(line)	
             philbank.append(Register(new_register.get('UserId'), new_register.get('Balance')))
 
 client.run(TOKEN)
